35c9377db1efebd3d04a6adcacb91b8fbc98509e
kami456 <kamranshahid218@gmail.com>
2021-01-25 03:21:56 -0800

    Assignment with new

---
+++ app.js
@@ -0,0 +1,129 @@
+// Chapter 21
+
+
+// var names = prompt("Enter your city name");
+// names = names.toUpperCase();
+// console.log(names);
+
+// Chapter 22
+
+// var mobileModel = prompt("Enter your statement");
+// if(mobileModel.length>=3){
+//     mobileModel=mobileModel.slice(0,3);
+//     console.log(mobileModel);
+// }
+
+// var mobile = prompt("Enter your statement");
+// if(mobile.length>=3){
+//     mobile=mobile.slice(4,6);
+//     console.log(mobile);
+// }
+
+
+// Chapter 23
+
+// var text = "pakistani"
+// var indexNum = text.indexOf("n");
+// console.log(indexNum);
+
+// var text = "pakistani"
+// var indexNum = text.indexOf("n");
+// console.log(indexNum);
+
+// var word = "The quick brown for jumps over the lazy dog";
+// var Ind = word.indexOf("the in given string");
+// console.log(Ind);
+
+// var replace = ("Hyder and Islam");
+// var a = replace.indexOf("Hyderabad");
+// var b = "Hyderabad";
+// console.log(a+b);
+
+// Chapter 24
+
+// var a = "pakistani";
+// console.log(a.charAt(3));
+
+// Chapter 25
+
+// var p = "Hyderabad";
+// var p1 = p.replace("Hyderabad","Hyder","Islam");
+// console.log(p1);
+
+// var r = "Ali and Sami are best friends.They play cricket and football together";
+// var r1 = r.replace(/and/g,"&");
+// console.log(r1);
+
+// Chapter 26
+
+// var user = prompt("Enter integer");
+// var num = 3.45214;
+// var round = Math.round(num);
+// console.log(round);
+
+// var user = prompt("Enter integer");
+// var num = 3.45214;
+// var round = Math.ceil(num);
+// console.log(round);
+
+
+// var user = prompt("Enter integer");
+// var num = 3.45214;
+// var round = Math.floor(num);
+// console.log(round);
+
+// var user = prompt("Enter integer");
+// var num = -2.678;;
+// var round = Math.round(num);
+// console.log(round);
+
+// var user = prompt("Enter integer");
+// var num = -2.678;
+// var round = Math.ceil(num);
+// console.log(round);
+
+// var user = prompt("Enter integer");
+// var num = -2.678;
+// var round = Math.floor(num);
+// console.log(round);
+
+
+// Chapter 27
+
+// var randomNumber = Math.random();
+// console.log(randomNumber);
+
+// var headsuser = prompt("Enter heads username");
+// var tailsuser = prompt("Enter tails username");
+// var toss = Math.random() * 2;
+// var floor = Math.floor(toss)
+// if(floor === 0){
+//     alert("Heads" + headsuser + " win the toss")
+// }else{
+//     alert("tails" + tailsuser + "win the toss")
+// }
+
+// Chapter 28
+
+// parseInt=("50");
+// console.log(parseInt);
+
+// parseInt=("50kgs");
+// console.log(parseInt);
+
+// parseInt=("50.2kgs");
+// console.log(parseInt);
+
+// Chapter 29
+
+// var num = "472";
+// console.log(num.toString());
+
+// var num = 35.36;
+// console.log(num.integerString());
+ 
+// Chapter 30
+
+// var num = 66.66666666666666;
+// var cont = num.toFixed(2);
+// console.log(cont);
